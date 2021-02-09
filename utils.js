17d6aff2eb3022fa879caffb080f462359ad93e1
Luncenok <idziejczak3@gmail.com>
2021-02-09 22:22:20 +0100

    Add getLoginMessageOrUndefine function

--- utils.js
+++ utils.js
@@ -3,3 +3,19 @@ module.exports.getArgs = async (message) => {
     args.shift()
     return args
 }
+
+module.exports.getLoginMessageOrUndefined = async (author) => {
+    const Keyv = require("keyv")
+    const keyv = new Keyv(process.env.PATH_TO_DATABASE)
+
+    let messageId = ""
+    messageId = await keyv.get(author.id)
+    let msgContent
+    await author.createDM()
+        .then(channel => channel.messages)
+        .then(messages => messages.fetch(messageId))
+        .then(msg => {
+            msgContent = msg.content
+        })
+    return msgContent
+}

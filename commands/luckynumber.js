cb830f44027884c15c8ae1ede6f72e5fb67f9f1a
Mateusz Idziejczak <idziejczak3@gmail.com>
2021-02-10 13:54:44 +0100

    Add lucky number command (#6)

---
+++ commands/luckynumber.js
@@ -0,0 +1,25 @@
+module.exports = {
+    name: "luckynumber",
+    description: "Pokazuje szczęśliwy numerek jeżeli dostępny",
+    aliases: ['ln', 'numerek', 'szczesliwynumerek', 'szczesliwy', 'lucky', 'number', 'sn'],
+    usage: 'luckynumber',
+    category: 'vulcan',
+    async execute(client, message) {
+        const uonet = require('../uonet')
+        const utils = require('../utils')
+
+        const loginProgressMessage = await message.channel.send("Logowanie... 0%")
+
+        const loginMessage = await utils.getLoginMessageOrUndefined(message.author)
+        if (loginMessage) {
+            await uonet.loginLogOn(loginMessage, loginProgressMessage).then((permcookiesymbolArray) => {
+                return uonet.getLuckyNumber(permcookiesymbolArray, loginProgressMessage)
+            }).then(luckyNumberText => {
+                loginProgressMessage.edit(luckyNumberText)
+            })
+        } else {
+            await loginProgressMessage.edit("Aby użyć tej komendy najpierw musisz się zalogować w wiadomości **prywatnej** do mnie. Po więcej informacji użyj komendy `help`")
+            await utils.removeFromDatabase(message.author.id)
+        }
+    }
+}
--- utils.js
+++ utils.js
@@ -1,3 +1,7 @@
+const discord = require('discord.js')
+const Keyv = require("keyv")
+const keyv = new Keyv(process.env.PATH_TO_DATABASE)
+
 module.exports.getArgs = async (message) => {
     let args = message.content.slice(process.env.PREFIX.length).split(/ +/);
     args.shift()
@@ -5,17 +9,24 @@ module.exports.getArgs = async (message) => {
 }
 
 module.exports.getLoginMessageOrUndefined = async (author) => {
-    const Keyv = require("keyv")
-    const keyv = new Keyv(process.env.PATH_TO_DATABASE)
-
     let messageId = ""
     messageId = await keyv.get(author.id)
-    let msgContent
-    await author.createDM()
-        .then(channel => channel.messages)
-        .then(messages => messages.fetch(messageId))
-        .then(msg => {
-            msgContent = msg.content
-        })
-    return msgContent
+    let msg
+    if (messageId)
+        await author.createDM()
+            .then(channel => channel.messages)
+            .then(messages => messages.fetch(messageId))
+            .then(message => {
+                msg = message
+            })
+            .catch(error => {
+                if (error instanceof discord.DiscordAPIError)
+                    msg = undefined
+                else console.log(error)
+            })
+    return msg
+}
+
+module.exports.removeFromDatabase = async (userId) => {
+    await keyv.delete(userId)
 }

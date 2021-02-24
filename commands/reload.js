d5e0fc0a8e7475b763735b0bbe483fbc3523b302
Kasza <34602864+ukaszogi@users.noreply.github.com>
2021-02-24 14:00:31 +0100

    Update reload.js
    żeby nie było spamu ready jak nic nie zrobił

--- commands/reload.js
+++ commands/reload.js
@@ -22,12 +22,13 @@ module.exports = {
             message.channel.send("Nieznana komenda `"+args[0]+"`.\nWpisz `"+ client.config.prefix +"help`, by otrzymać listę komend i przydatne linki");
         } else if(reloadCommand) {
             reloadCommandByName(reloadCommand.name);
+            console.log("Ready!")
         } else {
             const files = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
             files.forEach(file => {
                 reloadCommandByName(file.split('.')[0])
             });
+            console.log("Ready!")
         }
-        console.log("Ready!")
     }
 }

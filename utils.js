126fa2f10d93d6bee7fafb52fc323860d3859ea4
Kasza <34602864+ukaszogi@users.noreply.github.com>
2021-03-01 13:56:04 +0100

    Embed automation (#21)
    * Embed automation
    
    generateEmbed w utils i help.js z niego korzysta (YEEEEEE)
    
    * Update help.js
    
    Wywalić niepotrzebne consolelogi
    
    Co-authored-by: Mateusz Idziejczak <idziejczak3@gmail.com>

--- utils.js
+++ utils.js
@@ -74,6 +74,36 @@ module.exports.getTimetableFormattedText = (json, dayOfWeek) => {
     return `\`\`\`${timetableText}\`\`\``
 }
 
+module.exports.generateEmbed = (title, description, fields) => {
+    /**
+     * Generates embed for Vulcan'o'bot with color, author, timestamp and footer
+     * 
+     * @author Łukasz Szczyt
+     * @param {string} title Title of generated embed
+     * @param {string} description Description of generated embed
+     * @param {Array} fields Fields of generated embed. Array of objects {{name: *name*}: {value: *value*}}
+     */
+    const Embed = {
+        color: 0xd6d6d6,
+        title: `${title}`,
+        author: {
+            name: "Vulcan'o'bot",
+            icon_url: "https://s1.qwant.com/thumbr/0x380/0/5/5391fa8d72b7814188fd706773e8b335d12cb9505b3774e70eb952cd4a4a79/vector-volcano-eruption-illustration.jpg?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F000%2F216%2F030%2Foriginal%2Fvector-volcano-eruption-illustration.jpg&q=0&b=1&p=0&a=1"
+        },
+        description: `${description}`,
+        fields: fields,
+        timestamp: new Date(),
+        footer: {
+            text: "Vulcan'o'bot",
+            icon_url: "https://s1.qwant.com/thumbr/0x380/0/5/5391fa8d72b7814188fd706773e8b335d12cb9505b3774e70eb952cd4a4a79/vector-volcano-eruption-illustration.jpg?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F000%2F216%2F030%2Foriginal%2Fvector-volcano-eruption-illustration.jpg&q=0&b=1&p=0&a=1"
+        }
+    }
+    /**
+     * @todo icon get from assets folder, not from url
+     * @returns {Discord.MessageEmbed} Ready to send embed
+     */
+    return {embed: Embed}
+
 module.exports.getExamsFormattedText = (json) => {
     let examsText = ""
     json.forEach(tydzien => {

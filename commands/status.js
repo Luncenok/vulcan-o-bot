8fbd6313e589f4062e415264b1e4d979afe2e8e3
Mateusz Idziejczak <idziejczak3@gmail.com>
2021-03-05 11:25:53 +0100

    Better status (#31)
    * Add more checks in status command
    
    * Use embeds
    
    * Change embed color due to uonet status

--- commands/status.js
+++ commands/status.js
@@ -6,9 +6,87 @@ module.exports = {
     category: 'vulcan',
     async execute(client, message) {
         const fetch = require('node-fetch')
-        const checkmessage = await message.channel.send('Sprawdzanie...');
-        fetch('https://cufs.vulcan.net.pl/Default/Account/LogOn?ReturnUrl=%2F')
-            .then(res => checkmessage.edit(`Status: ${res.status}`))
+        const cheerio = require('cheerio')
+        const utils = require('../utils')
+        message.channel.startTyping()
+        let
+            isCufsTitleCorrect = false,
+            isCufsCodeCorrect = false,
+            isCufsTextCorrect = false,
+            isCufsPrzerwa = false,
+            cufsError = undefined,
+            isUczenTextCorrect = false,
+            isUczenCodeCorrect = false,
+            isUczenTitleCorrect = false,
+            isUczenPrzerwa = false,
+            uczenError = undefined
 
+        await fetch('https://uonetplus.vulcan.net.pl/warszawa')
+            .then(res => {
+                let cufsCode = res.status
+                if (cufsCode === 200) isCufsCodeCorrect = true
+                return res.text()
+            })
+            .then(res => {
+                let $ = cheerio.load(res)
+                let cufsTitle = $('title').text()
+                if (res.search('Zaloguj się') >= 0) isCufsTextCorrect = true
+                if (cufsTitle === "Dziennik UONET+") isCufsTitleCorrect = true
+                if (cufsTitle === "Przerwa techniczna") isCufsPrzerwa = true
+            })
+            .catch(error => {
+                console.log(error)
+                cufsError = error.message
+            })
+
+        await fetch('https://uonetplus-uczen.vulcan.net.pl/warszawa')
+            .then(res => {
+                let uczenCode = res.status
+                if (uczenCode === 200) isUczenCodeCorrect = true
+                return res.text()
+            })
+            .then(res => {
+                let $ = cheerio.load(res)
+                let uczenTitle = $('title').text()
+                if (res.search('Zaloguj się') >= 0) isUczenTextCorrect = true
+                if (uczenTitle === "Uczeń") isUczenTitleCorrect = true
+                if (uczenTitle === "Przerwa techniczna") isUczenPrzerwa = true
+            })
+            .catch(error => {
+                console.log(error)
+                uczenError = error.message
+            })
+
+        let cufsWorking = (isCufsCodeCorrect && isCufsTextCorrect && isCufsTitleCorrect),
+            uczenWorking = (isUczenCodeCorrect && isUczenTextCorrect && isUczenTitleCorrect)
+
+        const embed = utils.generateEmbed(
+            "Status dzienniczka vulcan.net.pl",
+            "Status dzienniczka vulcan.net.pl dla symbolu warszawa",
+            [{
+                name: cufsWorking ? "Strona logowania działa poprawnie" :
+                    "Wykryto błąd strony logowania:",
+                value: cufsWorking ? ":ok_hand:" :
+                    `Kod?: ${isCufsCodeCorrect ? ":white_check_mark:" : ":x:"}\n` +
+                    `Tytuł?: ${isCufsTitleCorrect ? ":white_check_mark:" : ":x:"}\n` +
+                    `Tekst?" ${isCufsTextCorrect ? ":white_check_mark:" : ":x:"}\n` +
+                    `Przerwa techniczna?: ${isCufsPrzerwa ? ":white_check_mark:" : ":x:"}\n` +
+                    `Error?" ${cufsError ? cufsError : ":x:"}`
+            }, {
+                name: uczenWorking ? "Nowy Uczeń działa poprawnie\n" : "Wykryto błąd w Nowym Uczniu:\n",
+                value: uczenWorking ? ":ok_hand:" :
+                    `Kod?: ${isUczenCodeCorrect ? ":white_check_mark:" : ":x:"}\n` +
+                    `Tytuł?: ${isUczenTitleCorrect ? ":white_check_mark:" : ":x:"}\n` +
+                    `Tekst?: ${isUczenTextCorrect ? ":white_check_mark:" : ":x:"}\n` +
+                    `Przerwa techniczna?: ${isUczenPrzerwa ? ":white_check_mark:" : ":x:"}\n` +
+                    `Error?: ${uczenError ? "\`uczenError\`" : ":x:"}`
+            }]
+        )
+
+        if (cufsWorking && uczenWorking)
+            embed.embed.color = "#9cfc9c"
+        else embed.embed.color = "#e73f48"
+        message.channel.stopTyping()
+        await message.channel.send(embed)
     }
 }

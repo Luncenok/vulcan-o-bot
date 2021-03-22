36b2471f79b20c91782cdb24970feefbbb549cc7
Luncenok <idziejczak3@gmail.com>
2021-03-22 22:29:48 +0100

    Add uptime and github link to info command

--- commands/info.js
+++ commands/info.js
@@ -7,6 +7,14 @@ module.exports = {
     async execute(client, message) {
         const utils = require('../utils');
         const version = require('../package.json').version;
+        let totalSeconds = (client.uptime / 1000);
+        let days = Math.floor(totalSeconds / 86400);
+        totalSeconds %= 86400;
+        let hours = Math.floor(totalSeconds / 3600);
+        totalSeconds %= 3600;
+        let minutes = Math.floor(totalSeconds / 60);
+        let seconds = Math.floor(totalSeconds % 60);
+        let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`
         await message.channel.send(utils.generateEmbed(
             "Informacje o bocie",
             "",
@@ -15,6 +23,12 @@ module.exports = {
                 value: `Serwery: ${client.guilds.cache.size}\n` +
                     `Kanały: ${client.channels.cache.size}\n` +
                     `Użytkownicy: ${client.users.cache.size}`
+            }, {
+                name: 'Uptime',
+                value: `${uptime}`
+            }, {
+                name: 'Github',
+                value: `https://github.com/Luncenok/vulcan-o-bot`
             }, {
                 name: 'Wersja',
                 value: `${version}`
